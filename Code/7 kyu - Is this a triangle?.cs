75b0b1cb49d2a3f2efd7cc5ef244b0ca4215b3ed
Rakovskij Stanislav <rakovskij.stanislav@gmail.com>
2017-11-11 13:27:31 +0300

    New Kata

--- Code/7 kyu - Is this a triangle?.cs
+++ Code/7 kyu - Is this a triangle?.cs
@@ -2,6 +2,7 @@ public class Triangle
 {
     public static bool IsTriangle(int a, int b, int c)
     {
+        //yes I could just compare a<b+c, b<a+c, c<a+b, but i was too sleepy)
         if (((a>=b)&&(a>=c)&&(a<b+c))||((b>=a)&&(b>=c)&&(b<a+c))||((c>=b)&&(c>=a)&&(c<b+a)))
             return true;
         return false;
